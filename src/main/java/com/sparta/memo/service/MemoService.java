dc9b3209323df1f52f869d8b84fde4378df95659
INYONG KIM <ohnyong87@gmail.com>
2023-09-01 12:39:26 +0900

    refactor : Replace findById Method to Repository

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -10,10 +10,15 @@ import java.util.List;
 
 public class MemoService {
 
-    private final JdbcTemplate jdbcTemplate;
+    //private final JdbcTemplate jdbcTemplate;//<- 이부분 중복된다.
+    private final MemoRepository memoRepository; //MemoRepository 타입의 객체를 생성 하는데~ 아래로.
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        //this.jdbcTemplate = jdbcTemplate;//<- 이부분 중복된다.
+        this.memoRepository = new MemoRepository(jdbcTemplate); //여기로, 이 객체가 생성 될 때,
+        // jdbc를 가진(인자->생성자의 매개변수로전달) 상태의 객체가 생성 된다. 이러면 아래 중복부분들이 모두 필요없어지게 된다.
+        // 왜냐하면 memoRepository.save(memo); 이렇게 jdbcTemplate를 가진 객체를 통해 save라는 메소드를 사용하고 있기 때문에
+        // jdbc를 가지고 있어서 코드 중복을 막게 되는 것이다.
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
@@ -21,7 +26,7 @@ public class MemoService {
         Memo memo = new Memo(requestDto);
 
         // DB 저장
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        //MemoRepository memoRepository = new MemoRepository(jdbcTemplate);//<- 이부분 중복된다. -> jdbc를 포함한 객체가 위에서 생성됬으니 메소드마다 생성 할 필요가 없다.
         Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
@@ -32,12 +37,12 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
         // DB 조회
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        //MemoRepository memoRepository = new MemoRepository(jdbcTemplate);//<- 이부분 중복된다.-> jdbc를 포함한 객체가 위에서 생성됬으니 메소드마다 생성 할 필요가 없다.
         return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        //MemoRepository memoRepository = new MemoRepository(jdbcTemplate);//<- 이부분 중복된다.-> jdbc를 포함한 객체가 위에서 생성됬으니 메소드마다 생성 할 필요가 없다.
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if (memo != null) {
@@ -51,7 +56,7 @@ public class MemoService {
     }
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        //MemoRepository memoRepository = new MemoRepository(jdbcTemplate);//<- 이부분 중복된다.-> jdbc를 포함한 객체가 위에서 생성됬으니 메소드마다 생성 할 필요가 없다.
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if (memo != null) {
@@ -63,22 +68,4 @@ public class MemoService {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
     }
-
-
-    public Memo findById(Long id) {
-        // DB 조회
-        String sql = "SELECT * FROM memo WHERE id = ?";
-
-        return jdbcTemplate.query(sql, resultSet -> {
-            if (resultSet.next()) {
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            } else {
-                return null;
-            }
-        }, id);
-    }
-
 }
